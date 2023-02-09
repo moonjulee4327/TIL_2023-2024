8265342c3303cba64eec0bf0b472728fecffa796
moonju lee <mjlee4327@gmail.com>
2023-02-09 23:21:42 +0900

    이스케이프 시퀀스 연습문제

--- baekjoon/baekjoon1000.java
+++ baekjoon/baekjoon1000.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
  * 두 정수 입력 후 두 수 더하기
  * 0 < A, B < 10
  */
-public class baekjoon1000 {
+public class Baekjoon1000 {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
