7e8e911d001a97d527e1dda0eda2a36bce6c04b7
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2023-11-15 18:19:00 +0900

    Update 구구단.java

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-
+        sc.close();
         for(int i = 1; i < 10; i++){
             System.out.println(num + " * " + i + " = " + (num * i));
         }
