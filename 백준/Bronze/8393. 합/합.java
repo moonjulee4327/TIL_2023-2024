90aa798d3af1362e8ec942634bf5358ef771f9b5
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2023-11-15 21:08:02 +0900

    [Bronze V] Title: 합, Time: 212 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        sc.close();
+        int result = 0;
+        for(int i = 1; i <= num; i++){
+            result += i;
+        }
+        System.out.println(result);
+    }
+}
