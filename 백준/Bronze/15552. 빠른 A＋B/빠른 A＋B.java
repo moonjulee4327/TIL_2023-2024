d38e8c376b9901641479e3c1ab3613ba5bfb5b50
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2023-11-17 23:36:44 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 828 ms, Memory: 221252 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
         String n = br.readLine();
         int num = Integer.parseInt(n);
@@ -16,11 +17,19 @@ public class Main {
 //            String[] inputNumberArr = inputNumber.split(" ");
 //            result = Integer.parseInt(inputNumberArr[0]) + Integer.parseInt(inputNumberArr[1]);
             // 방법 2
-            StringTokenizer st = new StringTokenizer(inputNumber);
-            result = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
-
-            bw.write(result + "\n");
+//            StringTokenizer st = new StringTokenizer(inputNumber);
+//            result = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+            // 방법 3
+            StringTokenizer st = new StringTokenizer(inputNumber, " ");
+            sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+            sb.append("\n");
+            // 방법 1, 2
+//            bw.write(result + "\n");
         }
-        bw.flush();
+        br.close();
+        // 방법 1, 2
+//        bw.flush();
+        // 방법 3
+        System.out.println(sb);
     }
 }
