07adb20efe483fda00e938085c1aedd3db1e0163
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2023-11-26 17:55:58 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -3,11 +3,11 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int[] numArr = new int[9];
         int max = 0;
-        int line = 0;
+        int index = 0;
         for(int i = 0; i < 9; i++){
             numArr[i] = Integer.parseInt(br.readLine());
         }
@@ -15,13 +15,14 @@ public class Main {
         for(int j = 0; j < numArr.length; j++){
             if(numArr[j] > max){
                 max = numArr[j];
-                line = j+1;
+                index = j+1;
             }
         }
 
         br.close();
-
-        bw.write(max + "\n" + line);
-        bw.close();
+        System.out.println(max);
+        System.out.println(index);
+//        bw.write(max + "\n" + index);
+//        bw.close();
     }
 }
