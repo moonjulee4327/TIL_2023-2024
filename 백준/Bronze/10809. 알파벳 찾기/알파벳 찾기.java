422e6b5eaf25d2a6318791df4f198524d11f037d
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2023-12-06 15:49:12 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 152 ms, Memory: 16060 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -2,22 +2,32 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        String[] strArr = br.readLine().split("");
-        br.close();
+        int[] numArr = new int[26];
+        for(int i = 0; i < numArr.length; i++){
+            numArr[i] = -1;
+        }
 
-        for(char i = 97; i < 123; i++){
-            for(int j = 0; j < strArr.length; j++){
-                if(strArr[j].equals(i+"")){
-                    sb.append(j + " ");
-                    break;
-                }else if (!strArr[j].equals(i+"") && j+1 == strArr.length){
-                    sb.append("-1 ");
+        String[] S = br.readLine().split("");
+
+        for(int j = 0; j < S.length; j++){
+            for(char k = 'a'; k <= 'z'; k++){
+                if(numArr[k - 'a'] != -1){
+                    continue;
+                }
+
+                if(S[j].equals(k+"")){
+                    numArr[k - 'a'] = j;
                 }
             }
         }
-        System.out.println(sb);
+
+        br.close();
+
+        for (int num : numArr) {
+            System.out.print(num + " ");
+        }
     }
 }
