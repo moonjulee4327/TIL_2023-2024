d82c6099bcc207f3d6266faf865432621f331cea
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2023-12-06 16:28:24 +0900

    [Bronze II] Title: 단어의 개수, Time: 256 ms, Memory: 20028 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+
+        System.out.println(st.countTokens());
+    }
+}
