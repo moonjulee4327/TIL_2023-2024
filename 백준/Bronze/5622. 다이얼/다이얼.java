7893e44de4bc3e4847b97f0453e9c16a3815fc95
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2023-12-08 00:37:06 +0900

    [Bronze II] Title: 다이얼, Time: 120 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -7,95 +7,30 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String str = br.readLine();
-        String[] strArr = str.split("");
+        br.close();
         int strLength = str.length();
-
         int result = 0;
-
+        
         for(int i = 0; i < strLength; i++){
             result += 2;
-            switch (strArr[i]){
-                case "A":
-                    result += 1;
-                    break;
-                case "B":
-                    result += 1;
-                    break;
-                case "C":
-                    result += 1;
-                    break;
-                case "D":
-                    result += 2;
-                    break;
-                case "E":
-                    result += 2;
-                    break;
-                case "F":
-                    result += 2;
-                    break;
-                case "G":
-                    result += 3;
-                    break;
-                case "H":
-                    result += 3;
-                    break;
-                case "I":
-                    result += 3;
-                    break;
-                case "J":
-                    result += 4;
-                    break;
-                case "K":
-                    result += 4;
-                    break;
-                case "L":
-                    result += 4;
-                    break;
-                case "M":
-                    result += 5;
-                    break;
-                case "N":
-                    result += 5;
-                    break;
-                case "O":
-                    result += 5;
-                    break;
-                case "P":
-                    result += 6;
-                    break;
-                case "Q":
-                    result += 6;
-                    break;
-                case "R":
-                    result += 6;
-                    break;
-                case "S":
-                    result += 6;
-                    break;
-                case "T":
-                    result += 7;
-                    break;
-                case "U":
-                    result += 7;
-                    break;
-                case "V":
-                    result += 7;
-                    break;
-                case "W":
-                    result += 8;
-                    break;
-                case "X":
-                    result += 8;
-                    break;
-                case "Y":
-                    result += 8;
-                    break;
-                case "Z":
-                    result += 8;
-                    break;
+            if(str.charAt(i) >= 'A' && str.charAt(i) <= 'C'){
+                result += 1;
+            } else if (str.charAt(i) <= 'F') {
+                result += 2;
+            } else if (str.charAt(i) <= 'I') {
+                result += 3;
+            } else if (str.charAt(i) <= 'L') {
+                result += 4;
+            } else if (str.charAt(i) <= 'O') {
+                result += 5;
+            } else if (str.charAt(i) <= 'S') {
+                result += 6;
+            } else if (str.charAt(i) <= 'V') {
+                result += 7;
+            }else {
+                result += 8;
             }
         }
-
         System.out.println(result);
     }
 }
