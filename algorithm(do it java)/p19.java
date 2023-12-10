20f6a629a1151332e13a0f6f022c9b367f8fa9de
moonju lee <mjlee4327@gmail.com>
2023-12-10 22:59:15 +0900

    단순수정

--- algorithm(do it java)/p19.java
+++ algorithm(do it java)/p19.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main3 {
+public class P19 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
