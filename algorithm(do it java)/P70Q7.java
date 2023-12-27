3693245489408990bb1404b1fb134f72047194c1
moonju lee <mjlee4327@gmail.com>
2023-12-27 23:06:57 +0900

    기수 변환 및 역순 출력

---
+++ algorithm(do it java)/P70Q7.java
@@ -0,0 +1,19 @@
+package doItJava;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P70Q7 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int ;
+        int cd;
+        int dno;
+        int retry;
+        char[] cno = new char[32];
+
+        System.out.println("10진수 기수를 변환합니다.");
+
+
+    }
+}
