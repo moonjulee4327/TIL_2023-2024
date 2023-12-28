e260ab886bdd6ee388cc0e9d44febf95c27a5cc9
moonju lee <mjlee4327@gmail.com>
2023-12-28 23:16:59 +0900

    기수 변환 과정 알고리즘 작성

--- algorithm(do it java)/P70Q6.java
+++ algorithm(do it java)/P70Q6.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class P70Q7 {
+public class P70Q6 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int no;
@@ -11,56 +11,44 @@ public class P70Q7 {
         int retry;
         char[] cno = new char[32];
 
-        System.out.println("10진수 기수를 변환합니다.");
-
+        System.out.println("10진수를 기수 변환합니다.");
         do {
             do {
-                System.out.print("변환할 음이 아닌 정수를 입력해주세요! : ");
+                System.out.print("변환하는 음이 아닌 정수 : ");
                 no = Integer.parseInt(br.readLine());
             }while (no < 0);
 
             do {
-                System.out.print("어떤 진수로 변환할까요? (ex.2 ~ 32) : ");
+                System.out.print("어떤 진수로 변환할까요? (2~36) : ");
                 cd = Integer.parseInt(br.readLine());
-            }while (cd < 2 || cd > 32);
+            }while (cd < 2 || cd > 36);
 
             dno = cardConv(no, cd, cno);
 
-            System.out.print("\n" + cd + "진수로는 ");
+            System.out.print(cd + "진수로는 ");
             for(int i = 0; i < dno; i++){
                 System.out.print(cno[i]);
             }
             System.out.print("입니다.");
 
-            System.out.print("\n한 번 더 실행하시겠습니까? (1. 예/0. 아니오) : ");
+            System.out.print("한 번 더 할까요? (1.예/0.아니요) : ");
             retry = Integer.parseInt(br.readLine());
         }while (retry == 1);
-
     }
 
     public static int cardConv(int x, int r, char[] d){
         int digits = 0;
         String dchar = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-        System.out.printf("%d | %10d", r, x);
         do {
-            System.out.printf("\n  + ----------");
-            if(x/r != 0){
-                System.out.printf("\n%d | %10d  ... %d", r, x / r, x % r);
-            }else {
-                System.out.printf("\n    %10d  ... %d", x / r, x % r);
-            }
-
             d[digits++] = dchar.charAt(x % r);
             x /= r;
         }while (x != 0);
-
         for(int i = 0; i < digits / 2; i++){
             char temp = d[i];
             d[i] = d[digits - i - 1];
             d[digits - i - 1] = temp;
         }
-
         return digits;
     }
 }
