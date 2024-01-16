2040ad5511159be9c85eb0babaa20d484bb4ec30
moonju lee <mjlee4327@gmail.com>
2024-01-16 20:59:27 +0900

    이진검색-클래스 메소드 사용

---
+++ algorithm(do it java)/P120Exam.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class P120Exam {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("요솟수를 입력하세요 : ");
+        int num = Integer.parseInt(br.readLine());
+
+        System.out.println("요소를 오름차순으로 입력하세요.");
+        int[] x = new int[num];
+        System.out.print("x[0] : ");
+        x[0] = Integer.parseInt(br.readLine());
+        for (int i = 1; i < num; i++){
+            do {
+                System.out.print("x[" + i + "] : ");
+                x[i] = Integer.parseInt(br.readLine());
+            }while (x[i] < x[i-1]);
+        }
+
+        System.out.print("검색 값을 입력해주세요 : ");
+        int key = Integer.parseInt(br.readLine());
+
+        int idx = Arrays.binarySearch(x, key);
+        if (idx < 0){
+            System.out.println("그 값의 요소가 없습니다.");
+        }else {
+            System.out.println(key + "은 x[" + idx + "]에 있습니다.");
+        }
+    }
+}
