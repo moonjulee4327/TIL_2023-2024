8d7e93508ff207c925f8efc07f652544c1bbc129
moonju lee <mjlee4327@gmail.com>
2024-01-26 22:32:19 +0900

    제네릭 stack 클래스 실행코드

---
+++ algorithm(do it java)/P144Q2.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P144Q2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        P144Q2Gstack s = new P144Q2Gstack(64);
+
+        while (true){
+            System.out.println("현재 데이터 수 : " + s.size() + "/" + s.capacity());
+            System.out.print("(1)푸시 (2)팝 (3)피크 (4)덤프 (5)검색 (6)모두 삭제 (7)정보 확인 (0)종료 : ");
+
+            int menu = Integer.parseInt(br.readLine());
+            if (menu == 0){
+                break;
+            }
+
+            String x;
+            switch (menu){
+                case 1:
+                    System.out.print("데이터 : ");
+                    x = br.readLine();
+                    try {
+                        s.push(x);
+                    }catch (P136ExamInstack.OverflowIntStackException e){
+                        System.out.println("스택이 가득 찼습니다.");
+                    }
+                    break;
+
+                case 2:
+                    try {
+                        System.out.println("팝한 데이터는 " + s.pop() + "입니다.");
+                    }catch (P136ExamInstack.EmptyIntStackException e){
+                        System.out.println("스택이 비어 있습니다.");
+                    }
+                    break;
+
+                case 3:
+                    try {
+                        System.out.println("피크한 데이터는 " + s.peek() + "입니다.");
+                    }catch (P136ExamInstack.EmptyIntStackException e){
+                        System.out.println("스택이 비어 있습니다.");
+                    }
+                    break;
+
+                case 4:
+                    s.dump();
+                    break;
+
+                case 5:
+                    System.out.print("검색할 값을 입력하세요 : ");
+                    x = br.readLine();
+
+                    int idx = s.indexOf(x);
+                    if (idx >= 0){
+                        System.out.println("해당 값은 꼭대기부터 " + (s.size() - idx) + "번째에 있습니다.");
+                    }else {
+                        System.out.println("해당 데이터는 없습니다.");
+                    }
+                    break;
+
+                case 6:
+                    s.clear();
+                    break;
+
+                case 7:
+                    System.out.println("용량 : " + s.capacity());
+                    System.out.println("데이터 수 : " + s.size());
+                    System.out.println("비어 " + (s.isEmpty() ? "있습니다." : "있지 않습니다."));
+                    System.out.println("가득 " + (s.isFull() ? "찼습니다." : "차 있지 않습니다."));
+                    break;
+            }
+        }
+    }
+}
