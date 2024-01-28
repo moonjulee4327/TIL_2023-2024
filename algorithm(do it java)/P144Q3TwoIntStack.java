561925f23ca2684d4a3a55cb8def42f07799d74d
moonju lee <mjlee4327@gmail.com>
2024-01-28 23:11:11 +0900

    하나의 배열로 앞뒤 스택 활용(2)

--- algorithm(do it java)/P144Q3TwoIntStack.java
+++ algorithm(do it java)/P144Q3TwoIntStack.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class P144Q3TwoIntStack {
@@ -55,21 +56,148 @@ public class P144Q3TwoIntStack {
                     throw new EmptyError();
                 }
                 result = stk[--ptrA];
+                break;
             case STACK_B:
                 if (ptrB <= 0){
                     throw new EmptyError();
                 }
                 result = stk[++ptrB];
+                break;
         }
         return result;
     }
 
-    public static void main(String[] args) {
+    public int peek(AorB ab) throws EmptyError{
+        int result = 0;
+        switch (ab){
+            case STACK_A:
+                if (ptrA <= 0){
+                    throw new EmptyError();
+                }
+                result = stk[ptrA - 1];
+                break;
+            case STACK_B:
+                if (ptrB <= 0){
+                    throw new EmptyError();
+                }
+                result = stk[ptrB - 1];
+                break;
+        }
+        return result;
+    }
+
+    public int indexOf(AorB ab){
+        switch (ab){
+            case STACK_A:
+                for (int i = ptrA - 1; i >= 0; i--){
+                    return i;
+                }
+                break;
+            case STACK_B:
+                for (int i = ptrB + 1; i < max; i++){
+                    return i;
+                }
+                break;
+        }
+        return -1;
+    }
+
+    public void clear(AorB ab){
+        switch (ab){
+            case STACK_A:
+                ptrA = 0;
+                break;
+            case STACK_B:
+                ptrB = max - 1;
+                break;
+        }
+    }
+
+    public int capacity(){
+        return max;
+    }
+
+    public int size(AorB ab){
+        switch (ab){
+            case STACK_A:
+                return ptrA;
+            case STACK_B:
+                return max - ptrB - 1;
+        }
+        return 0;
+    }
+
+    public boolean isEmpty(AorB ab){
+        switch (ab){
+            case STACK_A:
+                return ptrA <= 0;
+            case STACK_B:
+                return ptrB >= max - 1;
+        }
+        return true;
+    }
+
+    public boolean isFull(){
+        return ptrA >= ptrB + 1;
+    }
+
+    public void dump(AorB ab){
+        switch (ab){
+            case STACK_A:
+                if (ptrA <= 0){
+                    System.out.println("A스택은 비어 있습니다.");
+                }else {
+                    for (int i = 0; i < ptrA; i++){
+                        System.out.print(stk[i] + " ");
+                    }
+                }
+                break;
+            case STACK_B:
+                if (ptrB >= max - 1){
+                    System.out.println("B스택은 비어 있습니다.");
+                }else {
+                    for (int i = max - 1; i > ptrB; i--){
+                        System.out.print(stk[i] + " ");
+                    }
+                }
+                break;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        P144Q3TwoIntStack tis = new P144Q3TwoIntStack(10);
 
-        System.out.println(tis.push(AorB.STACK_A, 1));
+        System.out.print("스택 A와 B의 길이의 합을 설정해주세요. : ");
+        P144Q3TwoIntStack tis = new P144Q3TwoIntStack(Integer.parseInt(br.readLine()));
 
-        System.out.println(tis.push(AorB.STACK_B, 1));
+
+        while (true){
+            System.out.print("(1)푸시 (2)팝 (3)피크 (4)덤프 (5)검색 (6)모두 삭제 (7)스택 정보 확인 (8)덤프 (0)종료 : ");
+            int menu = Integer.parseInt(br.readLine());
+
+            if (menu == 0){
+                break;
+            }
+
+            AorB ab;
+            String abStr;
+            int x;
+            switch (menu){
+                case 1:
+                    System.out.print("스택 A, B를 선택해주세요. : ");
+                    abStr = br.readLine();
+                    if (abStr.equals("A")){
+                        ab = AorB.STACK_A;
+                    } else if (abStr.equals("B")) {
+                        ab = AorB.STACK_B;
+                    }else {
+                        System.out.println("올바른 입력을 해주세요.");
+                        continue;
+                    }
+                    System.out.print("데이터를 입력해주세요. : ");
+                    x = Integer.parseInt(br.readLine());
+                    tis.push(ab, x);
+            }
+        }
     }
 }
