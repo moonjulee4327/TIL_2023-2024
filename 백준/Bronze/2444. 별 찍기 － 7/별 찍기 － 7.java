711c40baab42b3738bc7d91caae8ec110050cf2d
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2024-08-31 01:35:01 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 268 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -4,48 +4,35 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        int num = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        for (int i = 1; i < 2 * num; i++){
-            if(i < num){
-                for (int j = 1; j < num; j++){
-                    if (j - 1 < num - i) {
-                        System.out.print(" ");
-                    }else {
-                        System.out.print("*");
-                    }
-                }
-                System.out.print("*");
-                for (int j = 1; j < num; j++){
-                    if (j > i - 1) {
-                        System.out.print("");
-                    }else {
-                        System.out.print("*");
-                    }
-                }
-            } else if (i == num) {
-                for (int j = 1; j < 2 * num; j++){
-                    System.out.print("*");
-                }
-            } else {
-                for (int j = num; j < 2 * num - 1; j++){
-                    if (j + 1 <= i) {
-                        System.out.print(" ");
-                    }else {
-                        System.out.print("*");
-                    }
-                }
-                System.out.print("*");
-                for (int j = 1; j < num; j++){
-                    if (i -num < num - j) {
-                        System.out.print("*");
-                    }else {
-                        System.out.print("");
-                    }
-                }
+        // 직관적으로 늘어나는 부분과 줄어드는 부분으로 나눠 생각
+        
+        // 별이 증가하는 부분
+        for (int i = 1; i <= n; i++){
+            // 출력을 보면 * 이후의 공백은 없다.
+            // 공백을 만든다.
+            for (int j = 0; j < n - i; j++){
+                System.out.print(" ");
             }
-            System.out.println("");
+            // 이후 별을 찍어준다.
+            for (int k = 0; k < 2 * i - 1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // 별이 감소하는 부분
+        for (int i = n - 1; i > 0; i--){
+            for (int j = 0; j < n - i; j++){
+                System.out.print(" ");
+            }
+            for (int k = 2 * i - 1; k > 0; k--){
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
