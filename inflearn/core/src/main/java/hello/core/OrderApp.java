c9df65f630ac2ac0433fc2c62e0764a3fbe2bbc5
moonju lee <mjlee4327@gmail.com>
2024-10-10 21:30:59 +0900

    fix: 할인 정책 변경

--- inflearn/core/src/main/java/hello/core/OrderApp.java
+++ inflearn/core/src/main/java/hello/core/OrderApp.java
@@ -3,10 +3,8 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
@@ -17,7 +15,7 @@ public class OrderApp {
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
 
         System.out.println("order = " + order);
     }
