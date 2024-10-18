831c8e1be752f2deeaf126c0d2474b79228f67ff
moonju lee <mjlee4327@gmail.com>
2024-10-18 23:06:58 +0900

    test: singleton 및 spring container 이해

---
+++ inflearn/core/src/test/java/hello/core/singleton/SingletonService.java
@@ -0,0 +1,16 @@
+package hello.core.singleton;
+
+public class SingletonService {
+
+    private static final SingletonService instance = new SingletonService();
+
+    private SingletonService() {}
+
+    public static SingletonService getInstance() {
+        return instance;
+    }
+
+    public void logic() {
+        System.out.println("싱글톤 객체 로직 호출");
+    }
+}
