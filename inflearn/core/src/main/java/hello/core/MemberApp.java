7c132ad4066a6ad638bfef83d7c79fa9630e95c8
moonju lee <mjlee4327@gmail.com>
2024-10-19 23:06:29 +0900

    rename: method 수정

--- inflearn/core/src/main/java/hello/core/MemberApp.java
+++ inflearn/core/src/main/java/hello/core/MemberApp.java
@@ -18,7 +18,7 @@ public class MemberApp {
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Member findMember = memberService.findById(1L);
+        Member findMember = memberService.findByMember(1L);
         System.out.println("new member = " + member.getName());
         System.out.println("find member = " + findMember.getName());
     }
