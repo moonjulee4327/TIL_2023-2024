4e66d15b003b7007f0989adbdd5efa3b3769df93
moonju lee <mjlee4327@gmail.com>
2024-10-22 19:53:25 +0900

    test: 의존관계 주입(필드 1)

--- inflearn/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ inflearn/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,27 +9,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
-
-    @Autowired
-    public void setMemberRepository(MemberRepository memberRepository) {
-        System.out.println("OrderServiceImpl.setMemberRepository");
-        this.memberRepository = memberRepository;
-    }
-
-    @Autowired
-    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-        System.out.println("OrderServiceImpl.setDiscountPolicy");
-        this.discountPolicy = discountPolicy;
-    }
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        System.out.println("1.OrderServiceImpl.OrderServiceImpl");
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+    @Autowired private MemberRepository memberRepository;
+    @Autowired private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
