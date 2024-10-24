30659cb6ab27c2490f7823c0e0e1a914bb5fdf73
moonju lee <mjlee4327@gmail.com>
2024-10-24 22:05:13 +0900

    feat: 자동 빈 충돌문제 이해

--- inflearn/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ inflearn/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,9 +1,12 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,7 +16,7 @@ public class OrderServiceImpl implements OrderService {
     private final DiscountPolicy discountPolicy;
 
     // @Autowired는 생성자가 하나만 존재할 경우 생략 가능
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
