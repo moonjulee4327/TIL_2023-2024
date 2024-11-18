24d94cb76a1954138b06516971e434002999d2b1
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2024-11-18 22:35:30 +0900

    [Silver II] Title: 에디터, Time: 340 ms, Memory: 49780 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -9,7 +9,6 @@ public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuffer sb = new StringBuffer();
 
         String str = br.readLine();
         int M = Integer.parseInt(br.readLine());
