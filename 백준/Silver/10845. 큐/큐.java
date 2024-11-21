be05f8f2f2f4d5b3b27d86468b7c503dad4864f3
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2024-11-21 21:04:27 +0900

    [Silver IV] Title: 큐, Time: 120 ms, Memory: 16976 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -7,62 +7,81 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static Queue<Integer> queue = new LinkedList<>();
+    private static int lastInt = 0;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        Queue<Integer> queue = new LinkedList<>();
 
-        int lastInt = 0;
         for (int i = 0; i < N; i++) {
             String str = br.readLine();
             StringTokenizer st = new StringTokenizer(str, " ");
             String cmd = st.nextToken();
-            
-            if (cmd.equals("push")){
-                lastInt = Integer.parseInt(st.nextToken());
-                queue.add(lastInt);
-            }else if (cmd.equals("pop")){
-                if(!queue.isEmpty()){
-                    String strNum = String.valueOf(queue.poll());
-                    bw.write(strNum);
-                }else {
-                    bw.write("-1");
-                }
-                bw.write("\n");
-            }else if (cmd.equals("size")){
-                String strNum = String.valueOf(queue.size());
-                bw.write(strNum);
-                bw.write("\n");
-            }else if (cmd.equals("empty")){
-                if (!queue.isEmpty()){
-                    bw.write("0");
-                }else {
-                    bw.write("1");
-                }
-                bw.write("\n");
-            }else if (cmd.equals("front")){
-                if (!queue.isEmpty()){
-                    String strNum = String.valueOf(queue.peek());
-                    bw.write(strNum);
-                }else {
-                    bw.write("-1");
-                }
-                bw.write("\n");
-            }else {
-                if (!queue.isEmpty()){
-                    String strNum = String.valueOf(lastInt);
-                    bw.write(strNum);
-                }else {
-                    bw.write("-1");
-                }
-                bw.write("\n");
+
+            switch (cmd) {
+                case "push":
+                    push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    bw.write(pop() + "\n");
+                    break;
+                case "size":
+                    bw.write(size() + "\n");
+                    break;
+                case "empty":
+                    bw.write(empty() + "\n");
+                    break;
+                case "front":
+                    bw.write(front() + "\n");
+                    break;
+                case "back":
+                    bw.write(back() + "\n");
+                    break;
             }
         }
 
         br.close();
         bw.flush();
         bw.close();
+    }
+
+    public static int back() {
+        if (queue.isEmpty()){
+            return -1;
+        }
+        return lastInt;
+    }
+
+    public static int front() {
+        if (queue.isEmpty()){
+            return -1;
+        }
+        return queue.peek();
+    }
+
+    public static int empty() {
+        if (queue.isEmpty()){
+            return 1;
+        }
+        return 0;
+    }
+
+    public static int size() {
+        return queue.size();
+    }
+
+    public static int pop() {
+        if (queue.isEmpty()){
+            return -1;
+        }
+        return queue.poll();
+    }
+
+    public static void push(int num) {
+        queue.add(num);
+        lastInt = num;
     }
 }
