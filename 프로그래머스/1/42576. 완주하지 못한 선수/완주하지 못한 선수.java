62d7f98756d9ec9df70350e85e5063fbf813fe8f
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-03-21 02:04:34 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 85.87 ms, Memory: 96 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        }
+        
+        for(String c : completion) {
+            map.put(c, map.get(c) - 1);
+        }
+        
+        for(String key : map.keySet()) {
+            if(map.get(key) != 0) {
+                return key;
+            }
+        }
+        
+        return answer;
+    }
+}
