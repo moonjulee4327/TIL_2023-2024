064c30ad25985d8022e9d702d93ef43a7dca39a3
moonju lee <106901006+moonjulee4327@users.noreply.github.com>
2025-03-21 18:39:37 +0900

    [level 1] Title: K번째수, Time: 0.53 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        int[] temp = {};
+        
+        for(int i = 0; i < commands.length; i++) {
+            temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2] - 1];
+        }
+        
+        return answer;
+    }
+}
